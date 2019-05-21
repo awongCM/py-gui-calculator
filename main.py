7b5e06a084441833454e02f7fd9f42da4dee596f
awongCM <awong.cm@gmail.com>
2019-05-22 00:07:23 +1000

    refactored to make pycalculator clas

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from tkinter import Tk, Label, Button
 
 
-class MyFirstGUIApp:
-    """docstring for MyFirstGUIApp."""
+class PyCalculator:
+    """docstring for PyCalculator."""
 
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
-
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
-
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-    def greet(self):
-        print('Greeting humans!')
+        master.title("Python GUI Calculator")
 
 
 root = Tk()
-my_gui_app = MyFirstGUIApp(root)
+py_calculator = PyCalculator(root)
 root.mainloop()
