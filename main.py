7bf910e901cbb5419d5eec2b44466a259ea73fa2
awongCM <awong.cm@gmail.com>
2019-05-22 12:16:06 +1000

    added screen widget

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button
+from tkinter import (Tk, Label, Button, Text)
 
 
 class PyCalculator:
@@ -7,6 +7,17 @@ class PyCalculator:
     def __init__(self, master):
         self.master = master
         master.title("Python GUI Calculator")
+
+        # screen widget
+        self.screen = Text(master, state='disabled', width=40,
+                           height=10, background="yellow", foreground="blue")
+
+        # screen widget positioning
+        self.screen.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
+        self.screen.configure(state='normal')
+
+        # initialize screen value
+        self.equation = ''
 
 
 root = Tk()
